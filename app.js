9678a05fef7ae9ef3455b2588dd7993c45c26994
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-04-30 21:15:26 +0500

    Fix: updated build script for Vercel

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const serverless = require('serverless-http'); // required for Vercel
+const bodyParser = require('body-parser');
 const session = require("express-session");
-const { getAllBooks, addUser, checkPassword, addInCart, getCartData, deleteFromCart } = require('./public/operations');
+const {
+    getAllBooks,
+    addUser,
+    checkPassword,
+    addInCart,
+    getCartData,
+    deleteFromCart
+} = require('../public/operations'); // Adjust path since this will live in /api now
 
 const app = express();
-const PORT = 3000;
 
 let userLoggedIn = false;
-let loginUser = {}
+let loginUser = {};
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,26 +27,22 @@ app.use(session({
 
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
     const books = getAllBooks();
-    res.render('home', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
+    res.render('home', { books, userLoggedIn, user: loginUser });
+});
 
 app.post('/signup', (req, res) => {
-    let { name, email, password } = req.body;
+    const { name, email, password } = req.body;
     addUser(name, email, password);
-
     res.redirect('/signin');
-})
+});
 
 app.post('/signin', (req, res) => {
     if ("email" in req.body && "password" in req.body) {
-
-        let email = req.body.email;
-        let password = req.body.password;
+        const { email, password } = req.body;
         const { value, user } = checkPassword(email, password);
-        // console.log(checkPassword(email, password));
-
         if (value) {
             userLoggedIn = true;
             loginUser = user;
@@ -49,60 +52,36 @@ app.post('/signin', (req, res) => {
             res.redirect("/signin?message=Incorrect username or password");
         }
     } else {
-        res.send("invalid request... username and password are required to login.")
+        res.send("invalid request... username and password are required to login.");
     }
-})
+});
 
 app.get('/allBooks', (req, res) => {
     const books = getAllBooks();
-    res.render('allBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
+    res.render('allBooks', { books, userLoggedIn, user: loginUser });
+});
 
-app.get('/classic', (req, res) => {
-    const books = getAllBooks();
-    res.render('classicBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
-
-app.get('/fantasy', (req, res) => {
-    const books = getAllBooks();
-    res.render('fantasyBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
-
-app.get('/motivational', (req, res) => {
-    const books = getAllBooks();
-    res.render('motivationalBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
-
-app.get('/science', (req, res) => {
-    const books = getAllBooks();
-    res.render('scienceBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
-
-app.get('/thriller', (req, res) => {
-    const books = getAllBooks();
-    res.render('thrillerBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
-
-app.get('/fiction', (req, res) => {
-    const books = getAllBooks();
-    res.render('fictionBooks', { books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
+// Other category routes...
+['classic', 'fantasy', 'motivational', 'science', 'thriller', 'fiction'].forEach((genre) => {
+    app.get(`/${genre}`, (req, res) => {
+        const books = getAllBooks();
+        res.render(`${genre}Books`, { books, userLoggedIn, user: loginUser });
+    });
+});
 
 app.get('/signin', (req, res) => {
-    res.render('signin-page')
-})
+    res.render('signin-page');
+});
 
 app.get('/signup', (req, res) => {
-    res.render('signup-page')
-})
+    res.render('signup-page');
+});
 
 app.get('/book-info', (req, res) => {
-    loginUser = loginUser;
-    userLoggedIn = userLoggedIn;
     const books = getAllBooks();
     const id = req.query.id - 1;
-    res.render('book-info-page', { id, books: books, userLoggedIn: userLoggedIn, user: loginUser })
-})
+    res.render('book-info-page', { id, books, userLoggedIn, user: loginUser });
+});
 
 app.post('/addToCart', (req, res) => {
     setTimeout(() => {
@@ -112,33 +91,29 @@ app.post('/addToCart', (req, res) => {
             addInCart(id, username);
             res.redirect(`/book-info?id=${req.body.id}`);
         } else {
-            res.redirect('/signin')
+            res.redirect('/signin');
         }
-
     }, 2000);
-
-})
-
+});
 
 app.get('/cart', (req, res) => {
     const books = getAllBooks();
     const cart = getCartData();
-    res.render('cart', { books: books, userLoggedIn: userLoggedIn, user: loginUser, cart: cart });
-})
+    res.render('cart', { books, userLoggedIn, user: loginUser, cart });
+});
 
 app.get('/delete-from-cart', (req, res) => {
     const id = req.query.id;
     deleteFromCart(id, loginUser.username);
     res.redirect('/cart');
-})
+});
 
 app.get('/logout', (req, res) => {
     userLoggedIn = false;
     loginUser = {};
     res.redirect('/');
-})
+});
 
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on http://localhost:${PORT}`);
-})
+// No app.listen here! Instead:
+module.exports = app;
+module.exports.handler = serverless(app); // Required for Vercel
